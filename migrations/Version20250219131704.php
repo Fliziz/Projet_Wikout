9746a1ece37f49ca2fcc28a6b85497128b7be5bb
Fliziz <axel@hotmail.com>
2025-02-28 11:13:40 +0100

    migration vide

--- migrations/Version20250219131704.php
+++ migrations/Version20250219131704.php
@@ -19,23 +19,11 @@ final class Version20250219131704 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_33497A212B48856B ON fiche_muscles');
-        $this->addSql('DROP INDEX UNIQ_33497A21924AC07F ON fiche_muscles');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_33497A212B48856B ON fiche_muscles (muscles_id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_33497A21924AC07F ON fiche_muscles (fiche_contenu_id)');
-        $this->addSql('ALTER TABLE fiches DROP FOREIGN KEY FK_459C25C9FB88E14F');
-        $this->addSql('ALTER TABLE fiches ADD CONSTRAINT FK_459C25C9FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id) ON DELETE CASCADE');
+        
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE fiches DROP FOREIGN KEY FK_459C25C9FB88E14F');
-        $this->addSql('ALTER TABLE fiches ADD CONSTRAINT FK_459C25C9FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id)');
-        $this->addSql('DROP INDEX UNIQ_33497A21924AC07F ON fiche_muscles');
-        $this->addSql('DROP INDEX UNIQ_33497A212B48856B ON fiche_muscles');
-        $this->addSql('CREATE INDEX UNIQ_33497A21924AC07F ON fiche_muscles (fiche_contenu_id)');
-        $this->addSql('CREATE INDEX UNIQ_33497A212B48856B ON fiche_muscles (muscles_id)');
+        
     }
 }
